0272c5ee14cbf0bd9c72d4cf60a93abd394f0a33
DavidV1go <devr240799@gmail.com>
2024-07-14 16:00:16 -0500

    Primero insercion

---
+++ database/Datos.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (id, name, email, email_verified_at, password, two_factor_secret, two_factor_recovery_codes, two_factor_confirmed_at, remember_token, created_at, updated_at) VALUES
+(1, 'administrador', 'admin@gmail.com', NULL, '$2y$12$B0ZbGOm/yvbJrxQK21uM4e.TV0MMG1nM/M.sj2WpDLu2aYDxM3tQW', NULL, NULL, NULL, NULL, '2024-07-14 22:04:54', '2024-07-14 22:04:54'),
+(2, 'Jessica Córdova', 'jcordova@gmail.com', NULL, '$2y$12$ODV.8tthgw333oO7L6eiGeBCOLGcjvtUGUR0vePfkJLirYr5NKruO', NULL, NULL, NULL, NULL, '2024-07-14 22:19:10', '2024-07-14 22:19:10'),
+(3, 'Lucila Briceño', 'lbriceno@gmail.com', NULL, '$2y$12$5NraSPu5yF5kk5jdmRdi/OAH4NHka0P0s3rf77wSx0A4AkK3iOdym', NULL, NULL, NULL, NULL, '2024-07-14 22:20:42', '2024-07-14 22:20:42'),
+(4, 'Alexandra Rodríguez', 'arodriguez@gmail.com', NULL, '$2y$12$IkDi7JXpG1fkp2AIa0TNQu4fEPXzzHINKOfohIQmIQbh1YyqU5X0K', NULL, NULL, NULL, NULL, '2024-07-14 22:21:25', '2024-07-14 22:21:25'),
+(5, 'David Vigo', 'dvigo@gmail.com', NULL, '$2y$12$/j9csEBmGe5./7Htl4XuHO7RI9Vk.2grqp0lXQSTKbcem6ds4A3KK', NULL, NULL, NULL, NULL, '2024-07-14 22:22:20', '2024-07-14 22:22:20');
+
+
+
+
+
